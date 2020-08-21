6a1edc7113a47f2bd56f2bb7803cd60ae881568f
“shun1012” <s.koyama1012@gmail.com>
2020-08-21 15:23:47 +0900

    lesson追加

---
+++ php/lesson.php
@@ -0,0 +1,30 @@
+<?php
+// class SampleCass {
+//   public $name = "中島";
+//
+//   public function getName() {
+//       return "name";
+//   }
+//
+// }
+//
+// $instance = new SampleCass();
+// echo $instance->name . "\n";
+// echo $instance->getName() . "\n";
+
+
+
+
+class sampleCass {
+  public $name = "小山";
+
+  public function getname() {
+    return "name";
+  }
+}
+$instance = new SampleCass();
+echo $instance->name . "\n";
+echo $instance->getname() . "\n";
+
+
+?>
