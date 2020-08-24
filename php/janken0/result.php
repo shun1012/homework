0b4e1457b210c215dfc9505129cc092a4de0a9f7
“shun1012” <s.koyama1012@gmail.com>
2020-08-24 09:39:51 +0900

    HWの提出

--- php/janken0/result.php
+++ php/janken0/result.php
@@ -7,78 +7,78 @@
   <body>
     <?php
 
-      // $janken = $_GET["janken"];
-      // echo 'あなたが出したのは'.$janken;
-      // echo "<br/>";
-      // echo "\n";
-      //
-      //
-      // $jankenPC = array("グー","チョキ","パー");
-      // $random = $jankenPC[ array_rand($jankenPC)];
-      // echo 'コンピューターが出したのは'.$random;
-      // echo "<br/>";
-      // echo "\n";
-      //
-      // // if ($janken == $random) {
-      // //   echo '引き分けです';
-      // // }
-      //
-      // switch ($jankenPC) {
-      //   case ($janken == "グー" && $random =="グー"):
-      //     echo '引き分けです';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
-      //   case ($janken == "グー" && $random =="チョキ"):
-      //     echo '勝ち';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
-      //   case ($janken == "グー" && $random =="パー"):
-      //     echo '負け';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
-      //   case ($janken == "チョキ" && $random =="チョキ"):
-      //     echo '引き分けです';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
-      //   case ($janken == "チョキ" && $random =="パー"):
-      //     echo '勝ち';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
-      //   case ($janken == "チョキ" && $random =="グー"):
-      //     echo '負け';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
-      //   case ($janken == "パー" && $random =="パー"):
-      //     echo '引き分けです';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
-      //   case ($janken == "パー" && $random =="グー"):
-      //     echo '勝ち';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
-      //   case ($janken == "パー" && $random =="チョキ"):
-      //     echo '負け';
-      //     echo "<br/>";
-      //     echo "\n";
-      //     break;
-      //
+      $janken = $_GET["janken"];
+      echo 'あなたが出したのは'.$janken;
+      echo "<br/>";
+      echo "\n";
+
+
+      $jankenPC = array("グー","チョキ","パー");
+      $random = $jankenPC[ array_rand($jankenPC)];
+      echo 'コンピューターが出したのは'.$random;
+      echo "<br/>";
+      echo "\n";
+
+      // if ($janken == $random) {
+      //   echo '引き分けです';
       // }
+
+      switch ($jankenPC) {
+        case ($janken == "グー" && $random =="グー"):
+          echo '引き分けです';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+        case ($janken == "グー" && $random =="チョキ"):
+          echo '勝ち';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+        case ($janken == "グー" && $random =="パー"):
+          echo '負け';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+        case ($janken == "チョキ" && $random =="チョキ"):
+          echo '引き分けです';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+        case ($janken == "チョキ" && $random =="パー"):
+          echo '勝ち';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+        case ($janken == "チョキ" && $random =="グー"):
+          echo '負け';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+        case ($janken == "パー" && $random =="パー"):
+          echo '引き分けです';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+        case ($janken == "パー" && $random =="グー"):
+          echo '勝ち';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+        case ($janken == "パー" && $random =="チョキ"):
+          echo '負け';
+          echo "<br/>";
+          echo "\n";
+          break;
+
+      }
 
 
 
