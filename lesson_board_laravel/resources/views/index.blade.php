0107139efb10c2761dd188a73a543afc96c1fa80
“shun1012” <s.koyama1012@gmail.com>
2020-09-14 14:17:46 +0900

    board_laravel追加

---
+++ lesson_board_laravel/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>paiza bbs</title>
+        <style>
+            body {padding: 10px;}
+        </style>
+    </head>
+    <body>
+        <h1>paiza bbs</h1>
+    </body>
+</html>
